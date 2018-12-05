17a7a9d7d497177464099b2d48d012a5691608fc
Delahaye Etienne <edelahaye@if501-208-05.insa-lyon.fr>
2018-12-05 09:43:48 +0100

    Add get_url.js

---
+++ get_url.js
@@ -0,0 +1,10 @@
+function get_url(request) {
+  /*
+   * request: the sparql request
+   * return a string (url)
+   */
+  request = String(request);
+  let url = "https://dbpedia.org/sparql?default-graph-uri=http%3A%2F%2Fdbpedia.org&query=";
+  let end = "&format=text%2Fhtml&CXML_redir_for_subjs=121&CXML_redir_for_hrefs=&timeout=30000&debug=on&run=+Run+Query+"
+  return (url+encodeURI(request)+end);
+}
