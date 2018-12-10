aa497c0ff43c6999f1477cc6e7b67e8e3047c55d
etienne-politinsa <etienne.politinsa@gmail.com>
2018-12-10 11:08:26 +0100

    Link suggestion to getAllMedallist

---
+++ getAllMedallist.js
@@ -0,0 +1,32 @@
+function get_url(request) {
+  /*
+   * request: the sparql request
+   * return a string (url)
+   */
+  request = String(request);
+  let url = "https://dbpedia.org/sparql?default-graph-uri=http%3A%2F%2Fdbpedia.org&query=";
+  let end = "&format=application%2Fsparql-results%2Bjson&CXML_redir_for_subjs=121&CXML_redir_for_hrefs=&timeout=30000&debug=on&run=+Run+Query+"
+  return (url+encodeURI(request)+end);
+}
+function httpGet(theUrl){
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+  xmlHttp.send( null );
+  return xmlHttp.responseText;
+}
+function getAllMedallist() {
+  var request = "select distinct ?athlete where{ {?athlete rdf:type yago:Medalist110305062.}UNION{?event dbo:bronzeMedalist ?athlete.}UNION{?event dbo:silverMedalist ?athlete.}UNION{?event dbo:goldMedalist ?athlete.}}";
+  var result = String(get_url(request));
+  var myObjectJSON = JSON.parse(httpGet(result));
+  var nameJson = [];
+  for (let i = 0; i<myObjectJSON.results.bindings.length; ++i){
+    let name = myObjectJSON.results.bindings[i].athlete.value;
+    name = name.substring(name.lastIndexOf("/")+1).replace('_', ' ');
+    let toRemove = name.substring(name.lastIndexOf('(')-1);
+    if(toRemove != name){
+      name = name.replace(toRemove,'');
+    }
+    nameJson.push(name);
+  }
+  return nameJson;
+}
