9e20b596a489bad5cdaa2c447b07b11ac3cd8d06
Delahaye Etienne <edelahaye@if501-208-05.insa-lyon.fr>
2018-12-12 10:34:02 +0100

    Résolution conflit

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function main() {
   var anchor = window.location.hash.substr(1);
   $("#athlete-name").html(anchor.split("_").join(" "));
-  $("#abstract").html("THIS IS GONNA BE THE ABSTRACT");
+  //$("#abstract").html("THIS IS GONNA BE THE ABSTRACT");
 }
 $(document).ready(main);
