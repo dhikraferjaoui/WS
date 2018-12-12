8dfe1fe5c2a4fcd91ec927fb6cb040d6ab41c5aa
Abdelaziz97 <37369167+Abdelaziz97@users.noreply.github.com>
2018-12-12 11:33:32 +0100

    Update suggestions.js

--- suggestions.js
+++ suggestions.js
@@ -1,5 +1,5 @@
 
-let athletes = getAllMedallist();
+/*let athletes = getAllMedallist();
 var input = document.getElementById("myInput");
 new Awesomplete(input, {
   list: athletes,
@@ -18,12 +18,26 @@ $( "#submit-btn" ).click(function() {
 	var myObjectJSON = JSON.parse(httpGet(result));
 	setImage(myObjectJSON.results.bindings[0].image.value);
 	setName(myObjectJSON.results.bindings[0].name.value);
-});
+});*/
 
-function setImage(imgsrc){
+function getInfo(name){
+	var request = "SELECT DISTINCT ?name, ?image, ?description WHERE { ?event rdf:type dbo:OlympicEvent{?event dbo:bronzeMedalist ?human.} UNION {?event dbo:silverMedalist ?human.}  UNION {?event dbo:goldMedalist ?human.} ?human dbo:thumbnail ?image. ?human rdfs:label ?name.  FILTER langMatches(lang(?name),'en')  FILTER contains(?name,'"+name+"')  ?human dbo:abstract ?description. FILTER langMatches(lang(?description),'en')}";
+	var result = String(get_url(request));
+	var myObjectJSON = JSON.parse(httpGet(result));
+	var infoJSON = {image:'',description:''};
+	var image = myObjectJSON.results.bindings[0].image.value;
+	var description = myObjectJSON.results.bindings[0].description.value;
+	if(image !== '')
+		infoJSON.image = image;
+	if(description !== '')
+		infoJSON.description = description;
+	return infoJSON;
+}
+
+/*function setImage(imgsrc){
 	document.getElementById("pic").src = imgsrc;
 }
 
 function setName(name){
 	document.getElementById("name").value = name;
-}
+}*/
