9d954de0774034f1859568bb2c85b49e96fac6f7
Delahaye Etienne <edelahaye@if501-208-05.insa-lyon.fr>
2018-12-12 11:45:21 +0100

    Bouton entrée

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
-function main() {
+function main () {
+  enterButton();
+  load_athlete();
+}
+
+function load_athlete() {
   var anchor = window.location.hash.substr(1);
   $("#athlete-name").html(anchor.split("_").join(" "));
   //$("#abstract").html("THIS IS GONNA BE THE ABSTRACT");
+  $("#sports").html(filterSports(anchor.split("_").join(" ")));
 }
 $(document).ready(main);
--- search.js
+++ search.js
@@ -1,6 +1,26 @@
+/*
+ * change #anchor when searching
+ */
 function search() {
   var artist = document.getElementById('myInput').value;
   artist = artist.split(" ").join("_");
   window.location.href = "athlete.html#" + artist;
-  main();
+  load_athlete();
+}
+
+/* from https://www.w3schools.com/howto/howto_js_trigger_button_enter.asp
+ * add listener to the search button
+ */
+function enterButton() {
+  var input = document.getElementById("myInput");
+  // Execute a function when the user releases a key on the keyboard
+  input.addEventListener("keyup", function(event) {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+      // Trigger the button element with a click
+      document.getElementById("submit-btn").click();
+    }
+  });
 }
